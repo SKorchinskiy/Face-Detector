6522fb17f8ab71eeea6c5c38b85bba90cf625fbc
SKorchinskiy <ddphyk@gmail.com>
2023-09-19 17:52:07 +0300

    + add new router / set cors / increase incoming traffic size to 1mb

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,11 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var detectionRouter = require("./routes/detection");
 
 var app = express();
 app.set("port", process.env.PORT || 8000);
@@ -16,14 +18,16 @@ app.set("port", process.env.PORT || 8000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/detect", detectionRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
