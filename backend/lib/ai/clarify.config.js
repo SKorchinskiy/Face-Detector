05098454a37539052f744e5bcfa0c1b0e41591a0
SKorchinskiy <ddphyk@gmail.com>
2023-09-19 17:39:57 +0300

    + add clarify for face recognition

---
+++ backend/lib/ai/clarify.config.js
@@ -0,0 +1,13 @@
+const PAT = process.env.AI_PAT;
+const USER_ID = process.env.AI_USER_ID;
+const APP_ID = process.env.AI_APP_ID;
+const MODEL_ID = process.env.AI_MODEL_ID;
+const MODEL_VERSION_ID = process.env.AI_MODEL_VERSION_ID;
+
+module.exports = {
+  PAT,
+  USER_ID,
+  APP_ID,
+  MODEL_ID,
+  MODEL_VERSION_ID,
+};
