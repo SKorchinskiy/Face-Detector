ce1099987592d6abd0bfd225bd590ee1c5ddc64e
SKorchinskiy <ddphyk@gmail.com>
2023-09-22 01:17:40 +0300

    - remove redundant fields from face-canvas component

--- frontend/src/components/face-canvas/face-canvas.component.tsx
+++ frontend/src/components/face-canvas/face-canvas.component.tsx
@@ -4,47 +4,43 @@ import { useEffect } from "react";
 
 type FaceCanvasProps = {
   id: number;
-  image_src: string;
-  image_width: number;
-  image_height: number;
-  margin_left: number;
-  margin_top: number;
+  box_width: number;
+  box_height: number;
+  box_left_margin: number;
+  box_top_margin: number;
   face_count: number;
 };
 
 export default function FaceCanvas({
   id,
-  image_src,
-  image_width,
-  image_height,
-  margin_left,
-  margin_top,
-  face_count,
+  box_width,
+  box_height,
+  box_left_margin,
+  box_top_margin,
 }: FaceCanvasProps) {
   useEffect(() => {
     const image = document.getElementById(
       "face-to-recognize"
     ) as HTMLImageElement;
-    cropRestrictedArea();
+    const tid = setInterval(() => {
+      cropRestrictedArea();
+      clearTimeout(tid);
+    }, 500);
     function cropRestrictedArea() {
       const canvas = document.getElementById(`face-canvas-${id}`);
-      canvas?.setAttribute("width", `${image_width}`);
-      canvas?.setAttribute("height", `${image_height}`);
+      canvas?.setAttribute("width", `${box_width}`);
+      canvas?.setAttribute("height", `${box_height}`);
       const context = (canvas as HTMLCanvasElement).getContext("2d");
-      const faceBox = document.getElementById(
-        `face-box-${id}`
-      ) as HTMLDivElement;
-      const ratio = image_width / image_height;
       context?.drawImage(
         image,
-        margin_left,
-        margin_top,
-        image_width + 20,
-        image_height + 20,
+        box_left_margin,
+        box_top_margin,
+        box_width + 20,
+        box_height + 20,
         0,
         0,
-        image_width,
-        image_height
+        box_width,
+        box_height
       );
     }
   }, []);
