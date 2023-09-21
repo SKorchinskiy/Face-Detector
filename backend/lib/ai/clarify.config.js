1ec6054ec8556a03545a89c1e30dfae08506e950
SKorchinskiy <ddphyk@gmail.com>
2023-09-22 01:05:21 +0300

    + update model set up process with abstractions

--- backend/lib/ai/clarify.config.js
+++ backend/lib/ai/clarify.config.js
@@ -1,8 +1,18 @@
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
 const PAT = process.env.AI_PAT;
 const USER_ID = process.env.AI_USER_ID;
 const APP_ID = process.env.AI_APP_ID;
 const MODEL_ID = process.env.AI_MODEL_ID;
 const MODEL_VERSION_ID = process.env.AI_MODEL_VERSION_ID;
+const GENERAL_MODEL_ID = process.env.AI_GENERAL_MODEL_ID;
+const GENERAL_MODEL_VERSION_ID = process.env.AI_GENERAL_MODEL_VERSION_ID;
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+
+metadata.set("authorization", "Key " + PAT);
 
 module.exports = {
   PAT,
@@ -10,4 +20,8 @@ module.exports = {
   APP_ID,
   MODEL_ID,
   MODEL_VERSION_ID,
+  GENERAL_MODEL_ID,
+  GENERAL_MODEL_VERSION_ID,
+  metadata,
+  stub,
 };
--- backend/lib/ai/index.js
+++ backend/lib/ai/index.js
@@ -1,59 +1,46 @@
-const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
-const clarify = require("./clarify.config");
-
-const stub = ClarifaiStub.grpc();
-
-const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key " + clarify.PAT);
-
-function formatDetectionData(detection_data) {
-  return detection_data.outputs[0].data.regions.map((face_region) => ({
-    bounding_box: face_region.region_info.bounding_box,
-    probability: face_region.value,
-  }));
-}
-
-async function getFaceDetectionData(image) {
-  try {
-    const response = await new Promise((resolve, reject) =>
-      stub.PostModelOutputs(
-        {
-          user_app_id: {
-            user_id: clarify.USER_ID,
-            app_id: clarify.APP_ID,
+function setUpImageModel(config) {
+  return async (image_url) => {
+    try {
+      const response = await new Promise((resolve, reject) =>
+        config.stub.PostModelOutputs(
+          {
+            user_app_id: {
+              user_id: config.USER_ID,
+              app_id: config.APP_ID,
+            },
+            model_id: config.MODEL_ID,
+            version_id: config.MODEL_VERSION_ID,
+            inputs: [
+              {
+                data: { image: { url: image_url, allow_duplicate_url: true } },
+              },
+            ],
           },
-          model_id: clarify.MODEL_ID,
-          version_id: clarify.MODEL_VERSION_ID,
-          inputs: [
-            { data: { image: { url: image, allow_duplicate_url: true } } },
-          ],
-        },
-        metadata,
-        (err, response) => {
-          if (err) {
-            reject(err);
-          }
+          config.metadata,
+          (err, response) => {
+            if (err) {
+              reject(err);
+            }
 
-          if (response.status.code !== 10000) {
-            reject(
-              "Post model outputs failed, status: " +
-                response.status.description
-            );
-          }
+            if (response.status.code !== 10000) {
+              reject(
+                "Post model outputs failed, status: " +
+                  response.status.description
+              );
+            }
 
-          resolve(response);
-        }
-      )
-    );
-    return {
-      detected_faces: formatDetectionData(response),
-    };
-  } catch (error) {
-    console.log({ error });
-    return error;
-  }
+            resolve(response);
+          }
+        )
+      );
+      return response;
+    } catch (error) {
+      console.log({ error });
+      return error;
+    }
+  };
 }
 
 module.exports = {
-  getFaceDetectionData,
+  setUpImageModel,
 };
