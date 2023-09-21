da0677c9687aab8073ba7158cfbf78db0f53767a
SKorchinskiy <ddphyk@gmail.com>
2023-09-21 03:09:35 +0300

    + update image processing to handle both image and file upload

--- backend/lib/image.helper.js
+++ backend/lib/image.helper.js
@@ -1,7 +1,5 @@
-async function createImageUrl(base64Image) {
+async function getImageData(formData) {
   try {
-    const formData = new FormData();
-    formData.append("image", base64Image);
     const response = await fetch(
       `https://api.imgbb.com/1/upload?key=${process.env.IMGBB_API_KEY}`,
       {
@@ -14,12 +12,36 @@ async function createImageUrl(base64Image) {
       buffer = buffer.concat(...chunk);
     }
     const { data } = JSON.parse(String.fromCharCode(...buffer));
-    return data.image.url;
+    return {
+      image_url: data.image.url,
+      bytes: data.size,
+      width: data.width,
+      height: data.height,
+      expiration: data.expiration,
+    };
   } catch (error) {
     console.log("hosting error", error);
   }
 }
 
-module.exports = {
-  createImageUrl,
-};
+async function getDataFromBinaryString(binaryString) {
+  const base64Image = Buffer.from(binaryString, "binary").toString("base64");
+  const formData = new FormData();
+  formData.append("image", base64Image);
+  return await getImageData(formData);
+}
+
+async function getDataFromUrlString(urlString) {
+  const formData = new FormData();
+  formData.append("image", urlString);
+  return await getImageData(formData);
+}
+
+async function getImageDataFromRequest(req) {
+  const imageData = await (req.body.imageUrl
+    ? getDataFromUrlString(req.body.imageUrl)
+    : getDataFromBinaryString(req.body.image.data));
+  return imageData;
+}
+
+module.exports = { getImageDataFromRequest };
