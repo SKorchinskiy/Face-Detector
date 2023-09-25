1a26f84670be4c16790020cc6004e8ef3c51a8ca
SKorchinskiy <ddphyk@gmail.com>
2023-09-26 02:37:57 +0300

    + add higher level updates to image gallery component

--- frontend/src/app/images/page.tsx
+++ frontend/src/app/images/page.tsx
@@ -3,15 +3,25 @@
 import styles from "./page.module.css";
 import { fetchData } from "@/utils/fetch.util";
 
-import type { ImageMetaData } from "../detect/image/[id]/page";
+import type { ImageMetaData } from "./(image)/[id]/page";
 import type { TagElement } from "@/components/tag/tag.component";
 import type { PaginationBarProps } from "@/components/pagination-bar/pagination-bar.component";
 
 import DetectionList from "@/components/detection-list/detection-list.component";
 import PaginationBar from "@/components/pagination-bar/pagination-bar.component";
-import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import TagList from "@/components/tag-list/tag-list.component";
+import React, {
+  MouseEvent,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import Sidebar from "@/components/side-bar/side-bar.component";
+import Image from "next/image";
+import TagsProvider, { TagsContext } from "@/context/tags.context";
+import { useRouter } from "next/navigation";
 
 type RecentDetections = {
   recentDetections: ImageMetaData[];
@@ -37,6 +47,11 @@ export default function Imges() {
     DEFAULT_RECENT_DETECTIONS
   );
   const [tags, setTags] = useState<Tag[]>([]);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const { selectedTags, applyTags, cancelUpdate } = useContext(TagsContext);
+  const router = useRouter();
+  const pathname = usePathname();
 
   const parsePageOrReturnDefault = (page: string | null) => {
     if (!page || !Number.isInteger(Number(page))) return 1;
@@ -44,9 +59,22 @@ export default function Imges() {
     return parsedPage > 0 ? parsedPage : 1;
   };
 
+  const onClickHandler = (event: MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLElement;
+    if (
+      isSidebarOpen &&
+      sidebarRef.current &&
+      !target.contains(sidebarRef.current) &&
+      !(sidebarRef.current as HTMLElement).contains(target)
+    ) {
+      cancelTagsHandler();
+      setIsSidebarOpen(false);
+    }
+  };
+
   useEffect(() => {
     const getTags = async () => {
-      const url = `http://localhost:8000/images/tags/${10}`;
+      const url = `http://localhost:8000/images/tags/${100}`;
       const topTags = await fetchData({ url });
       setTags(topTags);
     };
@@ -70,24 +98,85 @@ export default function Imges() {
     fetchImages();
   }, [searchParams]);
 
+  const applyTagsHandler = () => {
+    const appliedTags = applyTags();
+    const stringified = appliedTags.toString();
+    const params = new URLSearchParams(searchParams);
+    if (stringified) params.set("tags", stringified);
+    else params.delete("tags");
+    const query = decodeURIComponent(params.toString());
+    router.push(pathname + "?" + query);
+    setIsSidebarOpen(false);
+  };
+
+  const cancelTagsHandler = () => {
+    cancelUpdate();
+    setIsSidebarOpen(false);
+  };
+
   return (
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
+        position: "relative",
+        minHeight: "100%",
       }}
+      onClick={onClickHandler}
     >
-      <div className={styles["detections-container"]}>
-        <div className={styles["tags-container"]}>
-          <TagList tags={tags} />
+      {isSidebarOpen ? (
+        <>
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              background: "rgba(238, 238, 238, 0.4)",
+              zIndex: 20,
+            }}
+          ></div>
+          <Sidebar
+            sections={[
+              {
+                title: "tags",
+                elements: tags,
+              },
+            ]}
+            ref={sidebarRef}
+            applyHandler={applyTagsHandler}
+            cancelHandler={cancelTagsHandler}
+          />
+        </>
+      ) : (
+        <div
+          style={{
+            position: "absolute",
+            top: "40%",
+            left: 0,
+            zIndex: 30,
+            cursor: "pointer",
+          }}
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <Image alt="toggle" src="/arrow-right.svg" width={50} height={50} />
         </div>
-        <div className={styles["detections-data"]}>
-          <DetectionList detections={detections.recentDetections} />
-        </div>
-        <div className={styles["page-pagination"]}>
-          <PaginationBar pagination={detections.pagination} />
+      )}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <div className={styles["detections-container"]}>
+          <div className={styles["detections-data"]}>
+            <DetectionList detections={detections.recentDetections} />
+          </div>
+          <div className={styles["page-pagination"]}>
+            <PaginationBar pagination={detections.pagination} />
+          </div>
         </div>
       </div>
     </div>
