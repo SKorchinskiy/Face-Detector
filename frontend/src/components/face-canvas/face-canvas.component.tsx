c20453150f0c3f5682f5aae07923033f7e7400a2
SKorchinskiy <ddphyk@gmail.com>
2023-09-28 18:05:34 +0300

    + extrapolate canvas functionality into separate utility file

--- frontend/src/components/face-canvas/face-canvas.component.tsx
+++ frontend/src/components/face-canvas/face-canvas.component.tsx
@@ -1,50 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
+import { FaceCanvasProps, mountCanvas } from "@/utils/canvas.util";
+import { useEffect, useRef, useState } from "react";
 
-type FaceCanvasProps = {
-  id: number;
-  box_width: number;
-  box_height: number;
-  box_left_margin: number;
-  box_top_margin: number;
-};
+export default function FaceCanvas(props: Omit<FaceCanvasProps, "canvasId">) {
+  const canvasId = useRef(Math.round(Math.random() * 1000).toString());
 
-export default function FaceCanvas({
-  id,
-  box_width,
-  box_height,
-  box_left_margin,
-  box_top_margin,
-}: FaceCanvasProps) {
   useEffect(() => {
-    const image = document.getElementById(
-      "face-to-recognize"
-    ) as HTMLImageElement;
-
-    const tid = setInterval(() => {
-      cropRestrictedArea();
-      clearTimeout(tid);
-    }, 500);
-
-    function cropRestrictedArea() {
-      const canvas = document.getElementById(`face-canvas-${id}`);
-      canvas?.setAttribute("width", `${box_width}`);
-      canvas?.setAttribute("height", `${box_height}`);
-      const context = (canvas as HTMLCanvasElement).getContext("2d");
-      context?.drawImage(
-        image,
-        box_left_margin,
-        box_top_margin,
-        box_width,
-        box_height,
-        0,
-        0,
-        box_width,
-        box_height
-      );
-    }
-  }, []);
+    mountCanvas({ ...props, canvasId: canvasId.current });
+  }, [props]);
   return (
     <div
       style={{
@@ -52,8 +16,10 @@ export default function FaceCanvas({
         margin: 10,
       }}
     >
-      <span>Face {id + 1}: </span>
-      <canvas id={`face-canvas-${id}`} style={{ height: "100%" }}></canvas>
+      <canvas
+        id={`face-canvas-${canvasId.current}`}
+        style={{ height: "100%" }}
+      ></canvas>
     </div>
   );
 }
