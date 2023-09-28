70ca79cff8ae899bb83f4d335aef9d32425f11df
SKorchinskiy <ddphyk@gmail.com>
2023-09-28 17:53:11 +0300

    + add comparison router globally

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var detectionRouter = require("./routes/detection");
 const imageRouter = require("./routes/image");
+const compareRouter = require("./routes/compare");
 
 var app = express();
 app.set("port", process.env.PORT || 8000);
@@ -21,7 +22,7 @@ app.set("view engine", "jade");
 
 app.use(cors());
 app.use(logger("dev"));
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
@@ -30,6 +31,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/detect", detectionRouter);
 app.use("/images", imageRouter);
+app.use("/compare", compareRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
