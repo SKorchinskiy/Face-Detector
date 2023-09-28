b8d62d7b6950e61c06b8b6518fe88369262561a8
SKorchinskiy <ddphyk@gmail.com>
2023-09-28 18:05:50 +0300

    + extrapolate canvas-list functionality into separate utility file

---
+++ frontend/src/utils/image.utils.ts
@@ -0,0 +1,25 @@
+import {
+  BoundingBox,
+  ImageMetaData,
+} from "@/app/(side_pages)/images/(image)/[id]/page";
+
+export function getPixelBorderValues(
+  bounding_box: BoundingBox,
+  imageMetaData: ImageMetaData
+) {
+  const width =
+    (bounding_box["right_col"] - bounding_box["left_col"]) *
+    imageMetaData.width;
+  const height =
+    (bounding_box["bottom_row"] - bounding_box["top_row"]) *
+    imageMetaData.height;
+  const marginLeft = bounding_box["left_col"] * imageMetaData.width;
+  const marginTop = bounding_box["top_row"] * imageMetaData.height;
+
+  return {
+    width,
+    height,
+    marginLeft,
+    marginTop,
+  };
+}
