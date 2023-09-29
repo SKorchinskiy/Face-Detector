f2f1959378b813db0456203c605348dd42ad6de5
SKorchinskiy <ddphyk@gmail.com>
2023-09-29 20:56:17 +0300

    + update and refactor image page

--- frontend/app/_components/ui/particles/particles.component.tsx
+++ frontend/app/_components/ui/particles/particles.component.tsx
@@ -1,39 +1,29 @@
 "use client";
 
-import Particles from "react-particles";
 import { useCallback } from "react";
+import Particles from "react-particles";
 import type { Engine } from "tsparticles-engine";
 import { loadLinksPreset } from "tsparticles-preset-links";
 
-export default function ParticlesBackground() {
-  return <ParticlesContainer />;
-}
+const options = {
+  preset: "links",
+  zIndex: -10,
+  background: { color: "none" },
+  particles: {
+    color: { value: "#000" },
+    number: { value: 40 },
+    links: { color: { value: "#000" } },
+  },
+};
 
 function ParticlesContainer() {
   const customInit = useCallback(async (engine: Engine) => {
     await loadLinksPreset(engine);
   }, []);
 
-  const options = {
-    preset: "links",
-    zIndex: -10,
-    background: {
-      color: "none",
-    },
-    particles: {
-      color: {
-        value: "#000",
-      },
-      number: {
-        value: 40,
-      },
-      links: {
-        color: {
-          value: "#000",
-        },
-      },
-    },
-  };
-
   return <Particles options={options} init={customInit} />;
+}
+
+export default function ParticlesBackground() {
+  return <ParticlesContainer />;
 }
