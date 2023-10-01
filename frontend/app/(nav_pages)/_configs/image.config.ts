28a80cabb2d584567854d8e9670640a0526f2abd
SKorchinskiy <ddphyk@gmail.com>
2023-10-01 23:30:54 +0300

    + update image configuration to have strict type

--- frontend/app/(nav_pages)/_configs/image.config.ts
+++ frontend/app/(nav_pages)/_configs/image.config.ts
@@ -1,4 +1,19 @@
-export const ImageConfig = {
+import { MIMEType } from "util";
+
+export type FileType = {
+  description?: string;
+  accept: {
+    [key: typeof MIMEType.name]: string[];
+  };
+};
+
+export type ConfigOptions = Partial<{
+  multiple: boolean; // false
+  excludeAcceptAllOption: boolean; // false
+  types: Array<FileType>;
+}>;
+
+export const ImageConfig: ConfigOptions = {
   types: [
     {
       description: "Image",
