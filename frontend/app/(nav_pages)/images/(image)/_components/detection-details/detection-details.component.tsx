179c313cdd3af778f14797c4bda08cd2e2d21643
SKorchinskiy <ddphyk@gmail.com>
2023-10-06 17:51:40 +0300

    + update detection-details component with description-list component

--- frontend/app/(nav_pages)/images/(image)/_components/detection-details/detection-details.component.tsx
+++ frontend/app/(nav_pages)/images/(image)/_components/detection-details/detection-details.component.tsx
@@ -1,16 +1,42 @@
+"use client";
+
+import styles from "./detection-details.module.css";
+import DescriptionList from "../../../../_components/ui/description-list/description-list.component";
+import { DescriptionProps } from "../../../../_components/ui/description/description.component";
 import { ImageMetaData } from "../../[id]/page";
+import { useMemo } from "react";
+
+function formatDetectionDetails(imageMetaData: ImageMetaData) {
+  const user = "guest";
+  const expiration = imageMetaData.expiration || "-";
+  const size = `${
+    Math.round((imageMetaData.bytes / 1024 ** 2) * 10000) / 10000
+  }mb`;
+  const creation = imageMetaData.created_at;
+  const url = imageMetaData.url;
+  return { user, expiration, size, creation, url };
+}
 
 export default function DetectionDetails(imageMetaData: ImageMetaData) {
+  const data = useMemo(
+    () => formatDetectionDetails(imageMetaData),
+    [imageMetaData]
+  );
+
   return (
-    <div>
-      <p>user: guest</p>
-      <p>expires in: {imageMetaData.expiration || "-"}</p>
-      <p>
-        size(mb):{" "}
-        {Math.round((imageMetaData.bytes / 1024 ** 2) * 10000) / 10000}
-      </p>
-      <p>creation: {imageMetaData.created_at}</p>
-      <p>origin url: {imageMetaData.url}</p>
+    <div className={styles["details-container"]}>
+      <DescriptionList
+        list={[
+          ...Object.entries(data).map(
+            (res: [string, string | number]): DescriptionProps => {
+              return {
+                name: res[0],
+                value: res[1].toString(),
+              };
+            }
+          ),
+        ]}
+      />
     </div>
   );
 }
