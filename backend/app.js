f178aee0f747ee1229d8a814b405d42501d3cba3
SKorchinskiy <ddphyk@gmail.com>
2024-01-03 21:08:15 +0200

    + add origin to process variables

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN || "http://localhost:3000",
     credentials: true,
   })
 );
