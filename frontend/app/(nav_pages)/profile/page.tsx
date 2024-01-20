0b5da4a4f4141053ba02883cd94dd9d2d9dbf08c
SKorchinskiy <ddphyk@gmail.com>
2024-01-20 21:37:30 +0200

    + add statistics component

--- frontend/app/(nav_pages)/profile/page.tsx
+++ frontend/app/(nav_pages)/profile/page.tsx
@@ -5,19 +5,10 @@ import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../_context/user.context";
 import { fetchData } from "../_utils/fetch.util";
 import Image from "next/image";
-import DetectionTable from "./_components/detection-table/detection-table.component";
-import ComparisonTable from "./_components/comparison-table/comparison-table.component";
-import { RecentDetections } from "./_components/detection-table/detection-table.component";
-import { RecentComparisons } from "./_components/comparison-table/comparison-table.component";
+import Statistics from "./_components/statistics/statistics.component";
 
 export default function Profile() {
   const [statistics, setStatistics] = useState<any>({});
-  const [recentDetections, setRecentDetections] = useState<RecentDetections[]>(
-    []
-  );
-  const [recentComparisons, setRecentComparisons] = useState<
-    RecentComparisons[]
-  >([]);
   const [isSignedIn, setIsSignedIn] = useState(false);
   const userContext = useContext<any>(UserContext);
 
@@ -33,36 +24,6 @@ export default function Profile() {
       setStatistics(stats);
     };
     fetchStatistics();
-  }, []);
-
-  useEffect(() => {
-    const fetchRecentDetections = async () => {
-      // fix for dynamic user
-      const url = "http://localhost:8000/detect/1";
-      const options = { method: "GET" };
-      const detections = await fetchData({
-        url,
-        options,
-      });
-      setRecentDetections(detections);
-    };
-
-    fetchRecentDetections();
-  }, []);
-
-  useEffect(() => {
-    const fetchRecentComparisons = async () => {
-      // fix for dynamic user
-      const url = "http://localhost:8000/compare/1";
-      const options = { method: "GET" };
-      const comparisons = await fetchData({
-        url,
-        options,
-      });
-      setRecentComparisons(comparisons);
-    };
-
-    fetchRecentComparisons();
   }, []);
 
   return isSignedIn ? (
@@ -84,55 +45,7 @@ export default function Profile() {
               <p>Creation: {userContext?.currentUser?.created_at}</p>
             </div>
           </div>
-          <div className={styles["profile-page__info-blocks_element"]}>
-            <div>
-              <div>
-                <h2>All time statistics:</h2>
-                <p>
-                  Total detections:{" "}
-                  {statistics.detections ? statistics.detections.length : 0}
-                </p>
-                <p>
-                  Total comparisons:{" "}
-                  {statistics.comparisons ? statistics.comparisons.length : 0}
-                </p>
-              </div>
-              <div>
-                <h2>Last 30 days statistics:</h2>
-                <p>
-                  Total detections:{" "}
-                  {statistics?.recent ? statistics.recent.detections.length : 0}
-                </p>
-                <p>
-                  Total comparisons:{" "}
-                  {statistics?.recent
-                    ? statistics.recent.comparisons.length
-                    : 0}
-                </p>
-              </div>
-              <div>
-                <h2>Last 7 days statistics:</h2>
-                <p>
-                  Total detections:{" "}
-                  {statistics?.latest ? statistics.latest.detections.length : 0}
-                </p>
-                <p>
-                  Total comparisons:{" "}
-                  {statistics?.latest
-                    ? statistics.latest.comparisons.length
-                    : 0}
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles["profile-page__info-blocks_element"]}>
-            <h2>Recent Detections</h2>
-            <DetectionTable recentDetections={recentDetections} />
-          </div>
-          <div className={styles["profile-page__info-blocks_element"]}>
-            <h2>Recent Comparisons</h2>
-            <ComparisonTable recentComparisons={recentComparisons} />
-          </div>
+          <Statistics statistics={statistics} />
         </div>
       </div>
     </>
